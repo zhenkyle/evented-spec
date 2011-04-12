3bc45ae32c970308ead8eb42e4fdd45267b1c949
Mark Abramov <markizko@gmail.com>
2011-04-12 15:21:30 -0500

    Removed old and rusty em-spec shim

--- lib/evented-spec/em_spec_shim.rb
+++
@@ -1,6 +0,0 @@
-# Monkey patching EM to provide drop-in experience for legacy EM-Spec based examples.
-# Remember: monkey patching is dangerous, confusing and will come to haunt you.
-module EventMachine
-  Spec = EventedSpec::EMSpec
-  SpecHelper = EventedSpec::SpecHelper
-end
